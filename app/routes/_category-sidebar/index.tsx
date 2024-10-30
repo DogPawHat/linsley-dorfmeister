cdaccd7b9338eea8489e166561acecdb0a05251a
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2024-10-30 23:56:45 +0000

    images

--- app/routes/_category-sidebar/index.tsx
+++ app/routes/_category-sidebar/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { convexQuery } from "@convex-dev/react-query";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Image } from "@unpic/react";
 
 import { api } from "convex/_generated/api";
 
@@ -37,24 +36,33 @@ export default function Home() {
         <div key={collection.name}>
           <h2 className="text-xl font-semibold">{collection.name}</h2>
           <div className="flex flex-row flex-wrap justify-center gap-2 border-b-2 py-4 sm:justify-start">
-            {collection.categories.map((category) => (
-              <Link
-                key={category.name}
-                className="flex w-[125px] flex-col items-center text-center"
-                href={`/products/${category.slug}`}
-              >
-                {/* <Image
-                  loading={imageCount++ < 15 ? "eager" : "lazy"}
-                  decoding="sync"
-                  src={category.imageUrl ?? "/placeholder.svg"}
-                  alt={`A small picture of ${category.name}`}
-                  className="mb-2 h-14 w-14 border hover:bg-yellow-200"
-                  width={48}
-                  height={48}
-                /> */}
-                <span className="text-xs">{category.name}</span>
-              </Link>
-            ))}
+            {collection.categories.map((category) => {
+              const qs = new URLSearchParams([
+                ["source", category.imageUrl ?? "/placeholder.svg"],
+                ["w", "48"],
+                ["h", "48"],
+                ["q", "65"],
+              ]);
+
+              return (
+                <Link
+                  key={category.name}
+                  className="flex w-[125px] flex-col items-center text-center"
+                  href={`/products/${category.slug}`}
+                >
+                  <img
+                    loading={imageCount++ < 15 ? "eager" : "lazy"}
+                    decoding="sync"
+                    src={`/.netlify/images?${qs.toString()}`}
+                    alt={`A small picture of ${category.name}`}
+                    className="mb-2 h-14 w-14 border hover:bg-yellow-200"
+                    width={48}
+                    height={48}
+                  />
+                  <span className="text-xs">{category.name}</span>
+                </Link>
+              );
+            })}
           </div>
         </div>
       ))}
